5f319ce67186ecb06f43b567118753e25ac00edc
Kyle Mason <kyle.e.mason@outlook.com>
2024-02-11 14:03:19 -0500

    Refactor

--- src/Commit.ts
+++ src/Commit.ts
@@ -1,92 +1,65 @@
-import * as core from "@actions/core";
-import { exec, ExecOptions } from "@actions/exec";
-import { ActionContext } from "./ActionContext";
+import * as core from '@actions/core'
+import { exec, ExecOptions } from '@actions/exec'
+import { Inputs } from './Inputs'
 
 export const commit = async (
-    filesToCommit: string[],
-    actionContext: ActionContext,
-    message: string,
-    githubToken: string
+  filesToCommit: string[],
+  message: string,
+  inputs: Inputs,
+  newVersion: string
 ): Promise<void> => {
-    try {
-        core.info(`Committing changes with message "${message}".`);
-        const remoteRepository = `https://${actionContext.githubActor}:${githubToken}@github.com/${actionContext.githubRepository}.git`;
-        const tempBranch = `bump_tmp_${actionContext.headCommit.id}`;
+  try {
+    if (inputs.doCommit || inputs.doTag) {
+      const options = {
+        cwd: process.cwd(),
+        listeners: {
+          stdline: core.debug,
+          stderr: core.debug,
+          debug: core.debug
+        }
+      } as unknown as ExecOptions
 
-        const options = {
-            cwd: process.cwd(),
-            listeners: {
-                stdline: core.debug,
-                stderr: core.debug,
-                debug: core.debug
-            }
-        } as unknown as ExecOptions;
+      const authorEmail = 'github-actions[bot]@users.noreply.github.com'
+      const authorName = 'github-actions[bot]'
+      const branch = process.env.GITHUB_REF_NAME?.endsWith('/merge') ? process.env.GITHUB_HEAD_REF : process.env.GITHUB_REF_NAME // eslint-disable-line
+      const repository = process.env.GITHUB_REPOSITORY
+      const ghActor = process.env.GITHUB_ACTOR
+      const remoteRepo = `https://${ghActor}:${inputs.githubToken}@github.com/${repository}.git` // eslint-disable-line
 
-        core.info("git status");
-        await exec("git", ["status"], options);
+      core.info(`git config user.name "${authorName}"`)
+      await exec('git', ['config', 'user.name', `"${authorName}"`], options)
 
-        core.info(`git config user.name "${actionContext.pusher.name}"`);
-        await exec("git", ["config", "user.name", `"${actionContext.pusher.name}"`], options);
+      core.info(`git config user.email "${authorEmail}"`)
+      await exec('git', ['config', 'user.email', `"${authorEmail}"`], options)
 
-        core.info(`git config user.email "${actionContext.pusher.email}"`);
-        await exec("git", ["config", "user.email", `"${actionContext.pusher.email}"`], options);
-
-        // Issue #10
-        // While Git was updated from 2.33.0 to 2.33.1, need to specify how to reconcile divergent branches.
-        core.info(`git config pull.rebase false`);
-        await exec("git", ["config", "pull.rebase", `false`], options);
-
-        core.info(`git remote add publisher ${remoteRepository}`);
-        await exec("git", ["remote", "add", "publisher", remoteRepository], options);
-
-        core.info(`git branch ${tempBranch}`);
-        await exec("git", ["branch", tempBranch], options);
-
-        core.info(`git merge ${actionContext.branch}`);
-        await exec("git", ["merge", actionContext.branch], options);
-
-        // Issue #5
-        // Only commit changed files
+      if (inputs.doCommit) {
+        core.info('Committing Version')
         for (const file of filesToCommit) {
-            core.info(`git add "${file}"`);
-            await exec("git", ["add", file], options);
+          core.info(`git add "${file}"`)
+          await exec('git', ['add', file], options)
         }
-
-        core.info("git status");
-        await exec("git", ["status"], options);
-
-        try {
-            core.info(`git commit -m "${message}"`);
-            await exec("git", ["commit", "-m", `${message}`], options);
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                core.warning(`Warning: git commit failed. The error message is ${error.message}`);
-                core.warning(`Warning: git push will be skipped.`);
-            }
-            return;
+        core.info(`git commit -m "${message}"`)
+        await exec('git', ['commit', '-m', `${message}`], options)
+        if (inputs.doTag) {
+          core.info('Tagging Version')
+          core.info(`git tag ${newVersion}`)
+          await exec('git', ['tag', `${newVersion}`], options)
         }
-
-        core.info(`git checkout ${actionContext.branch}`);
-        await exec("git", ["checkout", actionContext.branch], options);
-
-        core.info(`git pull --no-edit --commit --strategy-option theirs publisher ${actionContext.branch}`);
-        await exec(
-            "git",
-            ["pull", "--no-edit", "--commit", "--strategy-option", "theirs", "publisher", actionContext.branch],
-            options
-        );
-
-        core.info(`git merge ${tempBranch}`);
-        await exec("git", ["merge", tempBranch], options);
-
-        core.info(`git push publisher ${actionContext.branch}`);
-        await exec("git", ["push", "publisher", actionContext.branch], options);
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            core.error(error.message);
-            core.setFailed(error.message);
+      }
+      if (inputs.doCommit) {
+        core.info(`git push ${remoteRepo} HEAD:${branch}`) // eslint-disable-line
+        await exec('git', ['push', `${remoteRepo}`, `HEAD:${branch}`], options) // eslint-disable-line
+        if (inputs.doTag) {
+          core.info(`git push ${remoteRepo} tag ${newVersion}`) // eslint-disable-line
+          await exec('git', ['push', `${remoteRepo}`, 'tag', `${newVersion}`], options) // eslint-disable-line
         }
-
-        process.exit(1);
+      }
     }
-};
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      core.error(error.message)
+      core.setFailed(error.message)
+    }
+    process.exit(1)
+  }
+}
--- src/Main.ts
+++ src/Main.ts
@@ -1,58 +1,56 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-import { ActionContext } from "./ActionContext";
-import { Bump } from "./Bump";
-import { commit } from "./Commit";
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { Bump } from './Bump'
+import { commit } from './Commit'
 
-async function bumpVersion(): Promise<void> {
-    core.info("dotnet-bump-version action is running...");
-    core.info("");
+async function bumpVersion (): Promise<void> {
+  core.info('dotnet-bump-version-action action is running...')
+  core.info('')
 
-    const inputs = (await import("./Inputs.js")).Inputs.current;
+  const inputs = (await import('./Inputs.js')).Inputs.current
 
-    const githubContext = github.context;
+  if (github.context.eventName !== 'push') {
+    core.info(`Github event is ${github.context.eventName} and not "push", exit.`) // eslint-disable-line
+    return
+  }
 
-    const actionContext = new ActionContext(githubContext);
+  const versionFiles = await inputs.getVersionFiles()
+  core.info('files to version:')
+  core.info(JSON.stringify(versionFiles))
+  core.info('')
 
-    // 只在 Github event 为 push 的时候生效
-    if (github.context.eventName !== "push") {
-        core.info(`Github event is ${github.context.eventName} and not "push", exit.`);
-        return;
+  const bumpedFiles: string[] = []
+  let toVersion = ''
+
+  versionFiles.forEach(file => {
+    const bump = new Bump(file, inputs)
+    if (bump.bump()) {
+      bumpedFiles.push(file)
+      if (toVersion === '') {
+        toVersion = bump.newVersion
+      }
     }
+  })
 
-    // 替换 version file 中的 version
-    const versionFiles = await inputs.getVersionFiles();
-    core.info("versionFiles:");
-    core.info(JSON.stringify(versionFiles));
-    core.info("");
+  if (inputs.needPushChanges && bumpedFiles.length > 0) {
+    await commit(bumpedFiles, 'Bump versions by dotnet-bump-version-action', inputs, toVersion)
+  }
 
-    // Issue #5
-    // Only commit changed files
-    let bumpedFiles = versionFiles.filter(file => {
-        const bump = new Bump(file, actionContext.headCommit.message, inputs);
-        return bump.bump();
-    });
-
-    // push changes
-    if (inputs.needPushChanges && inputs.githubToken && bumpedFiles.length > 0) {
-        await commit(bumpedFiles, actionContext, "Bump versions by dotnet-bump-version.", inputs.githubToken);
-    }
-
-    core.info("dotnet-bump-version action is finishing...");
+  core.info('dotnet-bump-version-action action is finished')
 }
 
-async function run(): Promise<void> {
-    try {
-        await bumpVersion();
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            core.setFailed(error.message);
-        }
+async function run (): Promise<void> {
+  try {
+    await bumpVersion()
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      core.setFailed(error.message)
     }
+  }
 }
 
 run().catch(error => {
-    if (error instanceof Error) {
-        core.setFailed(error.message);
-    }
-});
+  if (error instanceof Error) {
+    core.setFailed(error.message)
+  }
+})
